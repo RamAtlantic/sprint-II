533129b25581ee8a2d3bb5d58ac45abcd805a88d
RamAtlantic <arceramirom@gmail.com>
2024-05-30 21:32:52 -0300

    cambios cors

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Configura el servidor de desarrollo
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy todas las solicitudes a tu backend
-      "/": {
-        target: "https://api.curso.spazioserver.online",
-        changeOrigin: true,
-      },
-    },
-  },
 });
